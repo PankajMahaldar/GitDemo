cce1cf77cab05a084a4523f39e199ffcbb83e405
PankajMahaldar <Pankaj.mahaldar@outlook.com>
2024-05-14 10:45:59 +0530

    This is my first commit

---
+++ src/SeleniumJava.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String url = "https://www.google.com";
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+		System.out.println(driver.getTitle());
+		driver.quit();
+	}
+
+}
